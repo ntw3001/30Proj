a61b72e4b667a78aee2b2acee5f63eab93a5d239
Nick Westbrook <ntw3001@gmail.com>
2024-12-22 23:56:41 +0900

    did a little fancying up, and gave the submit button a true purpose

--- 1- Robot Query/main.js
+++ 1- Robot Query/main.js
@@ -6,6 +6,9 @@ submitBtn.disabled = true;
 
 const elements =  document.querySelectorAll('.element');
 const selectColor = document.getElementById('selectColor');
+const colorList = [];
+const resultMessage = document.getElementById('resultMessage');
+let chosenElement = null;
 
 elements.forEach(function (element) {
   const color = getRandomColor();
@@ -14,7 +17,8 @@ elements.forEach(function (element) {
   if (textBackground) {
     textBackground.innerHTML = color;
   }
-  selectColor.innerHTML = color;
+  colorList.push(color);
+  selectColor.innerHTML = colorList[Math.floor(Math.random() * colorList.length)];
 });
 
 
@@ -29,17 +33,24 @@ function getRandomColor() {
 
 elements.forEach(function (element) {
   element.addEventListener('click', function () {
-    const elementColor = element.querySelector('.element_text_bkgrnd');
-
-    if (elementColor.innerHTML === selectColor.innerHTML) {
-      alert('You pass the test');
-      checkbox.checked = true;
-      submitBtn.disabled = false;
-      submitBtn.classList.remove('btn-light');
-      submitBtn.classList.add('btn-success');
-    } else {
-      alert('01000111 01100101 01110100 00100000 01101111 01110101 01110100 00100000 01101111 01100110 00100000 01101000 01100101 01110010 01100101 00100000 01100011 01101100 01100001 01101110 01101011 01100010 01100001 01100111');
-      location.reload(true)
-    }
+    chosenElement = element.querySelector('.element_text_bkgrnd').innerHTML;
+      elements.forEach(function (el) {
+        el.classList.remove('chosen');
+      });
+    element.classList.toggle('chosen');
+    checkbox.checked = true;
+    submitBtn.disabled = false;
+    submitBtn.classList.remove('btn-light');
+    submitBtn.classList.add('btn-success');
   });
+});
+
+submitBtn.addEventListener('click', function () {
+  if (chosenElement === selectColor.innerHTML) {
+    alert('You pass the test');
+    location.reload(true)
+  } else {
+    alert('01000111 01100101 01110100 00100000 01101111 01110101 01110100 00100000 01101111 01100110 00100000 01101000 01100101 01110010 01100101 00100000 01100011 01101100 01100001 01101110 01101011 01100010 01100001 01100111');
+    location.reload(true)
+  }
 });
