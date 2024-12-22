2a7799db26c112b0bbd86c335afc300666190578
Nick Westbrook <ntw3001@gmail.com>
2024-12-22 23:16:22 +0900

    added a bit of class to the button

--- 1- Robot Query/main.js
+++ 1- Robot Query/main.js
@@ -34,8 +34,12 @@ elements.forEach(function (element) {
     if (elementColor.innerHTML === selectColor.innerHTML) {
       alert('You pass the test');
       checkbox.checked = true;
+      submitBtn.disabled = false;
+      submitBtn.classList.remove('btn-light');
+      submitBtn.classList.add('btn-success');
     } else {
       alert('01000111 01100101 01110100 00100000 01101111 01110101 01110100 00100000 01101111 01100110 00100000 01101000 01100101 01110010 01100101 00100000 01100011 01101100 01100001 01101110 01101011 01100010 01100001 01100111');
+      location.reload(true)
     }
   });
 });
