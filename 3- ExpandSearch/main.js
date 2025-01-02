82a3bc47b0e2b66402594cac1168d6f036128d23
Nick Westbrook <ntw3001@gmail.com>
2025-01-02 12:39:32 +0900

    set up the javascript, did a bit more styling to make the transition out pretty

---
+++ 3- ExpandSearch/main.js
@@ -0,0 +1,14 @@
+const searchBtn = document.getElementById('search-btn');
+
+const searchInput = document.querySelector(".search-input");
+
+console.log(searchBtn, searchInput);
+
+searchBtn.addEventListener('click', function() {
+  searchInput.classList.toggle('active-search');
+  console.log(searchInput);
+});
+
+document.querySelector("main").addEventListener('click', ()=> {
+  searchInput.classList.remove('active-search');
+});
