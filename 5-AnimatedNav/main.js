9388c4769e3eface6a56deab318f39c6329bf4f4
Nick Westbrook <ntw3001@gmail.com>
2025-01-08 04:22:19 +0900

    finished animation for menu dropdown, very pleasing

--- 5-AnimatedNav/main.js
+++ 5-AnimatedNav/main.js
@@ -6,5 +6,21 @@ menuToggle.addEventListener('click', function () {
     activeElements[active].classList.toggle('active');
   }
 
-  
+  const navBtnIcon= menuToggle.childNodes[1];
+  navBtnIcon.classList.toggle('fa-times');
+
+  const pageBG = document.querySelector('.main-container');
+  if(navBtnIcon.classList.contains('fa-times')) {
+    pageBG.style.filter = 'blur(8px)';
+  } else {
+    setTimeout(function(){
+      pageBG.style.filter = 'none';
+    }, 500);
+  }
+
+  const listItems = document.querySelectorAll('.main-nav-list a');
+  listItems.forEach (function (item, index) {
+    item.style.transition = `all 0.5s linear ${index * .3}s`;
+  });
+
 });
