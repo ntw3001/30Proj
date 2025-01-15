1ff6282232d40a62001582cb1f8dfa5ad120fd60
Nick Westbrook <ntw3001@gmail.com>
2025-01-16 01:13:49 +0900

    fixed up some flexbox arrangement, hover and focus effects on the tabs

--- 7-TabsMenu/main.js
+++ 7-TabsMenu/main.js
@@ -2,10 +2,7 @@ const mainContent = document.querySelector("main");
 const tabs = document.querySelectorAll(".btn");
 const content = document.querySelectorAll(".content");
 
-console.log(mainContent, tabs, content);
-
 mainContent.onclick = (e) => {
-  console.log(e.target.dataset);
   const id = e.target.dataset.id;
   if (id) {
     tabs.forEach(function (tab) {
