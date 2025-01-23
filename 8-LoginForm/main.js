7b1a3232e82c106529d009a10bb3b6a799236c1c
Nick Westbrook <ntw3001@gmail.com>
2025-01-24 03:05:18 +0900

    added the beginnings of a validation arrangement

--- 8-LoginForm/main.js
+++ 8-LoginForm/main.js
@@ -1,10 +1,35 @@
-const showPassword = document.querySelector('i.fa-eye');
+const loginContainer = document.querySelector('.login-container');
+const loginForm = document.getElementById('loginForm');
+const loginUsername = document.getElementById('loginUsername');
 const loginPassword = document.getElementById('loginPassword');
+const showPasswordIcon = document.querySelector('i.fa-eye');
 
-showPassword.addEventListener('click', function() {
+showPasswordIcon.addEventListener('click', function() {
   if (loginPassword.type === 'password') {
     loginPassword.type = 'text';
+    showPasswordIcon.style.color = 'red';
   } else {
     loginPassword.type = 'password';
+    showPasswordIcon.style.color = '#ddd';
   }
 });
+
+loginForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const username = loginUsername.value;
+  const password = loginPassword.value;
+  if (username === "" || username.length < 3) {
+    showError('That username is a losername');
+  } else {
+    showSuccess('You logged in, can\'t believe it');
+  }
+  if (password === "" || password.length < 6) {
+    showError('That password is an assword');
+  } else {
+    showSuccess('You logged in, can\'t believe it');
+  }
+});
+
+function showError(input, message) {
+  const formField = input.parentElement;
+}
