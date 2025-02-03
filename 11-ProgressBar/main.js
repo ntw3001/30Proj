bbde501606dfc7f5ac8039c3c58f20bacc01c8f0
Nick Westbrook <ntw3001@gmail.com>
2025-02-03 23:49:48 +0900

    added the functionality to reverse the previous functonality

--- 11-ProgressBar/main.js
+++ 11-ProgressBar/main.js
@@ -1,4 +1,4 @@
-const startBtn = document.getElementById('Start-btn');
+const goButton = document.getElementById('goButton');
 const elem = document.getElementById('myBar');
 const bgImg = document.getElementById('Bg-img');
 
@@ -13,22 +13,47 @@ let speed = 50;
 document.documentElement.style.setProperty('--speed', `${speed/10}s`);
 
 let i = 0;
+let width = 0;
 
-startBtn.addEventListener('click', () => {
+goButton.addEventListener('click', () => {
+  console.log(width);
+  console.log(i);
   if(i==0){
     i = 1;
-    let width = 0;
     let id = setInterval(frame, speed);
     function frame() {
       if (width >= 100) {
         clearInterval(id);
-        i = 0;
+        i = 1;
+        goButton.innerHTML = "Obfuscate";
+        goButton.disabled=false;
       } else {
+        goButton.disabled=true;
         width++;
         elem.style.width = width + "%";
         elem.innerHTML = width + "%";
         bgImg.classList.remove("blur");
         bgImg.classList.add("clear");
+      }
+    }
+  } else {
+    i=1;
+    let id = setInterval(reverseFrame, speed);
+    function reverseFrame() {
+      if (width <= 0) {
+        clearInterval(id);
+        i = 0;
+        goButton.innerHTML = "Clarify";
+        goButton.disabled=false;
+        bgImg.classList.add("blur");
+        bgImg.classList.remove("reblur");
+      } else {
+        goButton.disabled=true;
+        width--;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+        bgImg.classList.remove("clear");
+        bgImg.classList.add("reblur");
       }
     }
   }
