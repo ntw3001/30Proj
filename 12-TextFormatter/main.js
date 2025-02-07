cbd29c245ab620a6d9f140c7c283cafc0184b355
Nick Westbrook <ntw3001@gmail.com>
2025-02-07 12:23:28 +0900

    empowered the spongebob button, added a revert button, and all buttons now lose the active class when the input field is edited

--- 12-TextFormatter/main.js
+++ 12-TextFormatter/main.js
@@ -1,12 +1,6 @@
 const inputField = document.getElementById('input-field');
 const outputField = document.getElementById('output-field');
 
-inputField.addEventListener('keyup', getValue)
-
-function getValue() {
-    outputField.innerHTML = inputField.value;
-}
-
 const uppercaseBtn=document.querySelector('.uppercase');
 const lowercaseBtn=document.querySelector('.lowercase');
 const capitalizeBtn=document.querySelector('.capitalize');
@@ -14,6 +8,28 @@ const boldBtn=document.querySelector('.bold');
 const italicBtn=document.querySelector('.italic');
 const underlineBtn=document.querySelector('.underline');
 const spongebobBtn=document.querySelector('.spongebob');
+const revertBtn=document.querySelector('.revert');
+
+inputField.addEventListener('keyup', someonesTyping);
+
+function someonesTyping() {
+  getValue()
+  clear()
+}
+
+function getValue() {
+    outputField.innerHTML = inputField.value;
+}
+
+function clear() {
+  uppercaseBtn.classList.remove('active')
+  lowercaseBtn.classList.remove('active')
+  capitalizeBtn.classList.remove('active')
+  boldBtn.classList.remove('active')
+  italicBtn.classList.remove('active')
+  underlineBtn.classList.remove('active')
+  spongebobBtn.classList.remove('active')
+}
 
 uppercaseBtn.addEventListener('click', (e) => {
   e.target.classList.add('active')
@@ -55,7 +71,27 @@ underlineBtn.addEventListener('click', (e) => {
 });
 
 function spongeText () {
-  outputField.innerHTML = outputField.innerHTML.split('').map((char, index) => index % 2 === 0 ? char.toLowerCase() : char.toUpperCase()).join('');
+  let rawArray = outputField.innerHTML.split('')
+  let alphaArray = []
+  const nonAlpha = {}
+
+  const alphaCheck = /([a-zA-Z])+/g
+  rawArray.forEach((char, index) => {
+    if (!char.match(alphaCheck)) {
+      nonAlpha[index] = char
+    } else {
+      alphaArray.push(char)
+    }
+  })
+
+  alphaArray = alphaArray.map((char, index) =>
+    index % 2 === 0 ? char.toLowerCase() : char.toUpperCase());
+
+  let resultArray = rawArray.map((char, index) =>
+    nonAlpha[index] !== undefined ? char : alphaArray.shift()
+  );
+
+  outputField.innerHTML = resultArray.join('')
 };
 
 spongebobBtn.addEventListener('click', (e) => {
@@ -64,4 +100,15 @@ spongebobBtn.addEventListener('click', (e) => {
   lowercaseBtn.classList.remove('active')
   capitalizeBtn.classList.remove('active')
   return spongeText();
+});
+
+revertBtn.addEventListener('click', () => {
+  outputField.innerHTML = inputField.value;
+  uppercaseBtn.classList.remove('active')
+  lowercaseBtn.classList.remove('active')
+  capitalizeBtn.classList.remove('active')
+  boldBtn.classList.remove('active')
+  italicBtn.classList.remove('active')
+  underlineBtn.classList.remove('active')
+  spongebobBtn.classList.remove('active')
 });
