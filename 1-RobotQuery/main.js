0267ebd0e83637393ab9019724a71a866558566a
Nick Westbrook <ntw3001@gmail.com>
2025-02-10 00:11:08 +0900

    altered the robot test so it displays similar colours, raising the challange

--- 1-RobotQuery/main.js
+++ 1-RobotQuery/main.js
@@ -1,7 +1,5 @@
-// const checkbox = document.getElementById('checkbox');
 const submitBtn = document.querySelector('button[type=submit]');
 
-// checkbox.disabled = true;
 submitBtn.disabled = true;
 
 const elements =  document.querySelectorAll('.element');
@@ -9,6 +7,20 @@ const selectColor = document.getElementById('selectColor');
 const colorList = [];
 const resultMessage = document.getElementById('resultMessage');
 let chosenElement = null;
+let correctColor
+
+let colorArray= ["00", "00", "00"];
+const zeroPosition = Math.floor(Math.random() * 3);
+// shuffleArray(colorArray);
+
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
 elements.forEach(function (element) {
   const color = getRandomColor();
@@ -20,17 +32,21 @@ elements.forEach(function (element) {
   colorList.push(color);
   let randomColor = colorList[Math.floor(Math.random() * colorList.length)];
   randomColor = randomColor.trim();
-  // selectColor.innerHTML = randomColor;
   selectColor.style.backgroundColor = `${randomColor}`;
+  correctColor = randomColor;
 });
-
 
 function getRandomColor() {
   const letter = "0123456789ABCDEF";
   let color = "#";
-  for (let i = 0; i<6; i++) {
-    color += letter[Math.floor(Math.random() * 16)];
+  let arrayEntry = "";
+  for (let i = 0; i<2; i++) {
+    arrayEntry += letter[Math.floor(Math.random() * 16)];
   }
+  console.log(colorArray);
+  console.log(arrayEntry);
+  colorArray[zeroPosition] = arrayEntry;
+  color = color + colorArray.join("");
   return color
 }
 
@@ -41,7 +57,6 @@ elements.forEach(function (element) {
         el.classList.remove('chosen');
       });
     element.classList.toggle('chosen');
-    // checkbox.checked = true;
     submitBtn.disabled = false;
     submitBtn.classList.remove('btn-light');
     submitBtn.classList.add('btn-success');
@@ -49,11 +64,11 @@ elements.forEach(function (element) {
 });
 
 submitBtn.addEventListener('click', function () {
-  if (chosenElement === selectColor.innerHTML) {
-    alert('You pass the test');
-    location.reload(true)
+  if (chosenElement === correctColor) {
+    sessionStorage.setItem("result", "winner");
+    window.location.href = "result.html";
   } else {
-    alert('01000111 01100101 01110100 00100000 01101111 01110101 01110100 00100000 01101111 01100110 00100000 01101000 01100101 01110010 01100101 00100000 01100011 01101100 01100001 01101110 01101011 01100010 01100001 01100111');
-    location.reload(true)
+    sessionStorage.setItem("result", "loser");
+    window.location.href = "result.html";
   }
 });
