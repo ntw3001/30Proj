93528262e5a9c83369e2c3e47ef78e030bfbb483
Nick Westbrook <ntw3001@gmail.com>
2025-02-13 01:28:16 +0900

    made some neater animations for adding and deleting elements

--- 13-TaskList/main.js
+++ 13-TaskList/main.js
@@ -1,33 +1,60 @@
 const enterBtn = document.getElementById('enter');
 const input = document.getElementById('userInput');
 const ul = document.querySelector('ul');
-const item =document.querySelectorAll('li');
+const item = document.querySelectorAll('li');
 
 function inputLength() {
     return input.value.length;
 }
 
 function createListElement() {
-  const li = document.createElement('li');
-  li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
-  input.value = '';
 
-  function crossOut() {
+  function addListItem() {
+    const li = document.createElement('li');
+    li.prepend(document.createTextNode(input.value));
+    ul.prepend(li);
+    requestAnimationFrame(() => {
+      li.classList.add("newitem");
+    });
+    input.value = '';
+
+    function crossOut() {
       li.classList.toggle('done');
-  }
-  li.addEventListener('click', crossOut);
-  const deleteBtn = document.createElement('button');
-  deleteBtn.appendChild(document.createTextNode('X'));
-  li.appendChild(deleteBtn);
-
-  deleteBtn.addEventListener('click', deleteListItem);
-  function deleteListItem() {
-    li.classList.add('delete');
-    setTimeout(function() {
-      li.style.display = 'none';
+    }
+    li.addEventListener('click', crossOut);
+    const deleteBtn = document.createElement('button');
+    deleteBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(deleteBtn);
+    setTimeout(() => {
+      li.classList.remove("newitem");
     }, 500);
+
+    deleteBtn.addEventListener('click', deleteListItem);
+    function deleteListItem() {
+      li.classList.add('delete');
+      const itemsBelow = Array.from(ul.children).slice(Array.from(ul.children).indexOf(li) + 1);
+
+      itemsBelow.forEach(item => {
+        item.classList.add("limove-reverse");
+        setTimeout(() => {
+          item.classList.remove("limove-reverse");
+        }, 500);
+      });
+
+      setTimeout(() => {
+        li.remove();
+      }, 500);
+    };
   }
+
+  function liMove() {
+    ul.classList.add('limove');
+    setTimeout(function() {
+      ul.classList.remove('limove');
+      addListItem();
+    }, 500);
+    }
+    liMove();
 }
 
 enterBtn.onclick = () => {
