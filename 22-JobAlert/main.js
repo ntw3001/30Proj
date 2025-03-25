b788074efc13f22cbaf5b3a3745c21e0daaa11c3
Nick Westbrook <ntw3001@gmail.com>
2025-03-25 23:56:33 +0900

    finished the job looking page

---
+++ 22-JobAlert/main.js
@@ -0,0 +1,37 @@
+const getJob = document.getElementById('getJob');
+const slideBtns = document.querySelectorAll('.btn');
+const containers = document.querySelectorAll('.container');
+
+getJob.addEventListener('click', function () {
+    containers.forEach(function (container) {
+        container.style.display = "grid";
+    });
+  getJob.style.display = "none";
+});
+
+slideBtns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      const input = e.target.nextElementSibling;
+
+      function slide () {
+        if (input.classList.contains('slide-in')) {
+          input.classList.remove('slide-in');
+          input.classList.add('slide-out');
+
+          setTimeout (function () {
+          e.target.style.borderRadius="50%";
+          e.target.style.background="#f18d38";
+          }, 500);
+        } else {
+          e.target.style.borderRadius="50% 0 0 50%";
+          e.target.style.background="#6e46ae";
+
+          setTimeout (function () {
+          input.classList.remove('slide-out');
+          input.classList.add('slide-in');
+          }, 250);
+        }
+      }
+      slide();
+    });
+});
