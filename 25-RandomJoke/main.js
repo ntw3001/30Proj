f29ec221186d442ebadead7a97d524c865d736c8
Nick Westbrook <ntw3001@gmail.com>
2025-04-14 00:42:28 +0900

    substituted chuck norris joke api for non-chuck norris joke api

--- 25-RandomJoke/main.js
+++ 25-RandomJoke/main.js
@@ -1,17 +1,31 @@
-const displayJoke = document.getElementById("display-jokejoke");
-const button = document.querySelector("getJoke");
+const displayJoke = document.getElementById("display-joke");
+const button = document.querySelector("#getJoke");
+
+console.log(displayJoke);
+console.log(button);
 
 button.addEventListener("click", getRandomJoke);
 
 function getRandomJoke() {
   const ajax = new XMLHttpRequest();
-  const url = "https://api.chucknorris.io/jokes/random";
+  const url = "https://icanhazdadjoke.com/";
   ajax.open("GET", url, true);
+  console.log("Request sent");
+
+  ajax.setRequestHeader("Accept", "application/json");
 
   ajax.onreadystatechange = function () {
-    if (ajax.readyState == 4 && ajax.status == 200) {
-      const response = JSON.parse(ajax.responseText);
-      displayJoke.innerHTML = response.value;
+    if (ajax.readyState === 4 && ajax.status === 200) {
+      let response = JSON.parse(ajax.responseText);
+      displayJoke.innerHTML = response.joke;
+    } else {
+      ajax.onerror = onerror();
     }
   };
+  ajax.send();
+  button.innerText = "ANOTHER";
+}
+
+function onerror() {
+  displayJoke.innerHTML = "[[JOKE HIDDEN]] This was too funny for you.";
 }
