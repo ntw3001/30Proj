2ad667f1bed08830ee14426652cf177b6083f41d
Nick Westbrook <ntw3001@gmail.com>
2025-05-12 23:38:25 +0900

    did another tiny task list

---
+++ 31-SimpleTask/main.js
@@ -0,0 +1,23 @@
+const taskForm = document.getElementById('task-form');
+const taskList = document.getElementById('task-list');
+
+let taskIndex = 1;
+
+taskForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const taskInput = document.getElementById('task-input');
+  const taskValue = taskInput.value.trim();
+
+
+  if (taskValue) {
+    const taskItem = document.createElement('li');
+    taskItem.textContent = `${taskIndex} - ${taskValue}`;
+
+    taskList.appendChild(taskItem);
+    taskItem.classList.add ("task-item");
+    taskInput.value = '';
+    taskIndex++;
+  } else {
+    alert('Please enter a task');
+  }
+});
