dd1d5570d7285186a9a5e00a69c39fd2d9dbd85c
Nick Westbrook <ntw3001@gmail.com>
2025-05-13 23:47:12 +0900

    bit of javascript puzzlin

--- 0-Testbed/main.js
+++ 0-Testbed/main.js
@@ -1,17 +1,28 @@
-const players = [
-  {jersey: 45, name: "Peach Balmo", position: "Human shield", PPG: 9.1},
-  {jersey: 33, name: "Dom Broder", position: "Best boy", PPG: 7.2},
-  {jersey: 1352, name: "Jemmeifer Benglehauer", position: "Organ donor", PPG: 3},
-  {jersey: 4, name: "Lorcus Brint", position: "Point guard", PPG: 8.6}
-]
+// Map Function
 
-const list = document.querySelector (".players")
+const numbers = [1, 2, 3, 4, 5]
+const array = numbers.map((number) => {
+  return number*10
+});
 
-function populate () {
-  players.forEach(player => {
-    let template = `<li>${player.jersey}: ${player.name} - ${player.position}, PPG: ${Math.floor(player.PPG)}</li>`
-    list.insertAdjacentHTML("beforeend", template)
-  });
-}
+console.log(array);
 
-populate ()
+// Player List
+
+// const players = [
+//   {jersey: 45, name: "Peach Balmo", position: "Human shield", PPG: 9.1},
+//   {jersey: 33, name: "Dom Broder", position: "Best boy", PPG: 7.2},
+//   {jersey: 1352, name: "Jemmeifer Benglehauer", position: "Organ donor", PPG: 3},
+//   {jersey: 4, name: "Lorcus Brint", position: "Point guard", PPG: 8.6}
+// ]
+
+// const list = document.querySelector (".players")
+
+// function populate () {
+//   players.forEach(player => {
+//     let template = `<li>${player.jersey}: ${player.name} - ${player.position}, PPG: ${Math.floor(player.PPG)}</li>`
+//     list.insertAdjacentHTML("beforeend", template)
+//   });
+// }
+
+// populate ()
